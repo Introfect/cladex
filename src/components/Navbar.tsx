1db88624c1a208a3b001a78c87424070f6940493
Aayushman Pratap <aayushmanpratap333@gmail.com>
2026-01-19 00:37:21 +0530

    added name in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   return (
     <header className="w-full sticky right-0 left-0 bg-white top-0 py-5 px-4 backdrop-blur-lg z-[100] flex items-center border-b-[1px] justify-between">
       <aside className="flex items-center gap-[2px] bg-gray-300 px-4 py-2">
-        <p className="text-xl font-bold text-red-500">LOGO</p>
+        <p className="text-xl font-bold text-red-500">Cladx</p>
       </aside>
       <nav
        
